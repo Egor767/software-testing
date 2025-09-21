12a5e14c34a893760f5df3b5b98f22df0b89fa4c
Egor Musiyak <musiyak26@mail.ru>
2025-09-21 10:09:02 +0600

    Successful behave-tests

--- features/steps/auth.py
+++ features/steps/auth.py
@@ -12,5 +12,5 @@ def step_when_current_attempt(context, current_attempt):
 def step_then_expected_access(context, expected_access):
     actual_access = (context.current_attempt<=context.max_attempts)
     expected_access_bool = expected_access.lower() == 'true'
-    assert actual_access == expected_access_bool
+    assert actual_access == expected_access_bool # ==
 
