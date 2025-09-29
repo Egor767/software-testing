9d3432721d53b69d70a6dd1415f0d71b437cd2b0
Egor Musiyak <musiyak26@mail.ru>
2025-09-29 15:57:25 +0600

    successful commit

--- features/steps/bank.py
+++ features/steps/bank.py
@@ -1,5 +1,6 @@
 from behave import *
 
+
 class Account:
     def __init__(self, balance: float) -> None:
         self.balance = balance
@@ -9,10 +10,12 @@ class Account:
             raise Exception('The cashout amount is more than balance')
         self.balance -= amount
 
+
 # Given
 @given('the account balance is {balance}')
 def step_impl(context, balance):
     context.account = Account(int(balance))
+
 
 # When
 @when('i attempt to cashout {cashout_amount}')
@@ -23,16 +26,19 @@ def step_impl(context, cashout_amount):
     except Exception:
         context.cashout_successful = False
 
+
 # Then
 # exceeding
 @then('the cashout should be declined')
 def step_impl(context):
     assert context.cashout_successful is False
 
+
 # successful
 @then('the cashout should be successful')
 def step_impl(context):
     assert context.cashout_successful is True
+
 
 @then('final account balance should be {expected_balance}')
 def step_impl(context, expected_balance):
--- features/steps/coffe_machine.py
+++ features/steps/coffe_machine.py
@@ -1,12 +1,15 @@
 from behave import *
 
+
 @given('required volume of milk is {needed_volume} ml')
 def step_given_needed_volume(context, needed_volume):
     context.needed_volume = int(needed_volume)
 
+
 @given('actual volume of milk is {current_volume} ml')
 def step_given_current_volume(context, current_volume):
     context.current_volume = int(current_volume)
+
 
 @when('attempting to make cappuccino')
 def step_when_attempt_make_cappuccino(context):
@@ -16,9 +19,11 @@ def step_when_attempt_make_cappuccino(context):
         context.attempt_declined = False
         context.current_volume -= context.needed_volume
 
+
 @then('the attempt should be declined')
 def step_then_attempt_declined(context):
     assert context.attempt_declined, "Attempt was not declined but should be"
+
 
 @then('final volume of milk in coffee machine should be {expected_volume} ml')
 def step_then_final_volume(context, expected_volume):
